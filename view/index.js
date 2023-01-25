c492ef9b68033456a6d3952ba97b0a89721f3073
pitossomo <pitossomo@gmail.com>
2023-01-24 23:48:21 -0300

    altera dados do modal de filmes dinamicamente

--- view/index.js
+++ view/index.js
@@ -20,20 +20,27 @@ window.addEventListener("load", () => {
       filmes = response.data;
 
       console.log(`GET filmes`, filmes);
-      filmes.forEach((filme) => {
+      filmes.forEach((filme, index) => {
         const div = document.createElement("div");
         div.classList.add("item");
+        div.setAttribute('data-index',`${index}`)
         div.innerHTML = `
           <h2>${filme.titulo}</h2>
           <p>Release: ${filme.release}</p>
           <p>Elenco: ${filme.cast}</p>
           <p>Gênero: ${filme.genre}</p>
         `;
+        div.addEventListener('click', e => populateModal(index))
         container.appendChild(div);
       });
     })
     .catch((error) => console.error(error));
 });
+
+const populateModal = index => {
+  document.querySelector('.modal-title').innerHTML = filmes[index].titulo
+  document.querySelector('.modal-body').innerHTML = filmes[index].enredo || `Esse filme foi lançado em ${filmes[index].release}. A história é muito massa, de ${filmes[index].genre}, mas depois eu conto. Tem muita gente legal: ${filmes[index].cast}.`  
+}
 
 burguer.addEventListener("click", () => {
   upbar.classList.toggle("upcross");
